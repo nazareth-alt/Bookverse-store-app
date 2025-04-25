c97f28cce8bdba9ccfc0e539feb0c7e2feb433bf
nazareth hlana <nazarethhlana1@gmail.com>
2025-04-25 06:57:57 +0200

    url update

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,7 +1,5 @@
 const getBaseUrl = () => {
-    return import.meta.env.DEV
-      ? "http://localhost:5000"
-      : "https://your-backend-url.vercel.app";
+    return "https://bookverse-store-app.vercel.app";
   };
   
   export default getBaseUrl;
